f0c79ffb1179662a3f77bd830ad1700ba3cfd251
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-05-11 18:34:34 +0900

    [feature] #3 set yolov8n

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from ultralytics import YOLO
 
 app = FastAPI()
 
+global model
+
+@app.on_event("startup")
+def on_startup():
+    global model
+    model = YOLO('detect/yolov8n.pt')
+
+@app.get("/test")
+async def detection():
+    results = model(['detect/bus.jpg'])
+    for result in results:
+        print(result)
+        #result.show()
+        #result.save(filename='detect/output_image.jpg')
+    return {"success!"}
 
 @app.get("/")
 async def root():
