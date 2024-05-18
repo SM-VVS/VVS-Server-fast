871200cd2eab7848dafceefa276338ff5e1cad3f
jhjalison01 <alison01@naver.com>
2024-05-19 01:08:31 +0900

    [feature] #3 connect with front

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+import io
+from pydantic import BaseModel
 from ultralytics import YOLO
 import base64
 from io import BytesIO
@@ -39,3 +41,51 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+class ImageData(BaseModel):
+    base64Image: str
+
+
+@app.post("/upload")
+async def upload_image(image_data: ImageData):
+    try:
+        # Base64 문자열을 디코딩
+        image_bytes = base64.b64decode(image_data.base64Image)
+
+        # 이미지 파일을 열어 확인
+        image = Image.open(io.BytesIO(image_bytes))
+
+        results = model([image])
+
+        for result in results:
+            boxes = result.boxes  # Boxes object for bbox outputs
+            # Print box coordinates
+            print("Box coordinates:")
+            for box in boxes:
+                print(box)
+
+        json_results = results_to_json(results)
+        # print("Json results:")
+        # print(json_results)
+        # return json_results
+
+        return {"message": "Image uploaded successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid image data") from e
+
+
+def results_to_json(results):
+    return [
+        [
+            {
+                "class": int(box.cls),
+                "class_name": model.model.names[int(box.cls)],
+                "bbox": [x for x in box.xyxy.tolist()[0]],  # convert bbox results to int from float
+                "confidence": float(box.conf),
+            }
+            for box in result.boxes
+        ]
+        for result in results
+    ]
