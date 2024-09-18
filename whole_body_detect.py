7ce3573267aab87000f5bfa246eb190062c26a23
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-09-18 20:57:24 +0900

    [feature] #16 detect whole body (without coordinate)

--- whole_body_detect.py
+++ whole_body_detect.py
@@ -10,7 +10,6 @@ router = APIRouter()
 # get YOLOv8 model
 yolo_weights_path = 'yolov8n.pt'
 model = YOLO(yolo_weights_path)
-# model.predict(source="0", show=True, stream=True, classes=0)
 
 class ImageData(BaseModel):
     base64_image: str
@@ -27,6 +26,7 @@ async def detect_whole_body(image_data: ImageData):
         image_np = np.frombuffer(image_bytes, np.uint8)
         # Decode the numpy array to an image
         img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        img_height, img_width, _ = img.shape
 
         if img is None:
             raise ValueError("이미지를 디코딩할 수 없습니다.")
@@ -36,46 +36,60 @@ async def detect_whole_body(image_data: ImageData):
             img = cv2.flip(img, 1)
 
         # apply
-        results = model(img)
+        results = model.predict(source=img, show=False, classes=0)
         message = ""
-        ppl = True
+        boxes = []
 
         # 결과 시각화
         for result in results:
             for bbox in result.boxes:
                 class_id = int(bbox.cls[0])  # 클래스 ID
                 conf = bbox.conf[0]  # 신뢰도
-                if class_id == 0:
-                    if conf > 0.7:
-                        ppl = False
-                        x1, y1, x2, y2 = map(int, bbox.xyxy[0])  # 좌표
-                        #label = f"{class_id}: {conf:.2f}"  # 레이블 텍스트
-                        #print(x1, ", ", y1, ", ", x2, ", ", y2)
+                x1, y1, x2, y2 = map(int, bbox.xyxy[0])  # 좌표
+                boxes.append({
+                    "x1": x1,
+                    "y1": y1,
+                    "x2": x2,
+                    "y2": y2,
+                    "class_id": class_id,
+                    "conf": conf
+                })
 
-                        # 바운딩 박스
-                        # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                        # cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-                        if x1 == 0:
-                            message = "카메라를 왼쪽으로 옮기십시오"
-                        elif y1 == 0:
-                            message = "카메라를 아래로 옮기십시오"
-                        elif x2 == 470:
-                            message = "카메라를 오른쪽으로 옮기십시오"
-                        elif y2 == 630:
-                            message = "카메라를 위로 옮기십시오"
-
-                    else:
-                        message = "얼굴 인식 정확도가 낮습니다. 카메라를 조정하십시오."
-        if ppl:
+        plen = len(boxes)
+        if plen == 0:
             message = "얼굴을 찾을 수 없습니다. 카메라를 조정하십시오."
+        else:
+            sorted(boxes, key=lambda x: x['conf'], reverse=True)
+            bbox = result.boxes[0]
+            message = f"화면에 {len(boxes)}명이 있습니다."
 
-        # 이미지 표시
-        # cv2.imshow('Detection Results', img)
-        # cv2.waitKey(1)
-        # cv2.destroyAllWindows()
+            # if bbox['conf'] > 0.7:
+            #     # 10% margin
+            #     margin = 0.1
+            #     left_bound = img_width * margin
+            #     right_bound = img_width * (1 - margin)
+            #     top_bound = img_height * margin
+            #     bottom_bound = img_height * (1 - margin)
+            #
+            #     if bbox['x1'] < left_bound:
+            #         message = "카메라를 왼쪽으로 옮기십시오"
+            #     elif bbox['x2'] > right_bound:
+            #         message = "카메라를 오른쪽으로 옮기십시오"
+            #     elif bbox['y1'] < top_bound:
+            #         message = "카메라를 위로 옮기십시오"
+            #     elif bbox['y2'] > bottom_bound:
+            #         message = "카메라를 아래로 옮기십시오"
+            #     else:
+            #         message = "피사체가 중앙에 있습니다"
 
-        #print("message:", message)
+                # 이미지 표시
+                # cv2.imshow('Detection Results', img)
+                # cv2.waitKey(1)
+                # cv2.destroyAllWindows()
+
+            # else:
+            #     message = "얼굴 인식 정확도가 낮습니다. 카메라를 조정하십시오."
+
         return {"message": message}
 
     except Exception as e:
