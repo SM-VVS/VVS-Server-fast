8011da22eca689e837a86cfccedee133ec0faad2
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-09-22 01:59:36 +0900

    [feature] #16 set coordinate

--- whole_body_detect.py
+++ whole_body_detect.py
@@ -57,38 +57,38 @@ async def detect_whole_body(image_data: ImageData):
 
         plen = len(boxes)
         if plen == 0:
-            message = "얼굴을 찾을 수 없습니다. 카메라를 조정하십시오."
+            message = "전신을 찾을 수 없습니다. 카메라를 조정하십시오."
         else:
             sorted(boxes, key=lambda x: x['conf'], reverse=True)
-            bbox = result.boxes[0]
-            message = f"화면에 {len(boxes)}명이 있습니다."
+            bbox = boxes[0]
+            # message = f"화면에 {len(boxes)}명이 있습니다."
 
-            # if bbox['conf'] > 0.7:
-            #     # 10% margin
-            #     margin = 0.1
-            #     left_bound = img_width * margin
-            #     right_bound = img_width * (1 - margin)
-            #     top_bound = img_height * margin
-            #     bottom_bound = img_height * (1 - margin)
-            #
-            #     if bbox['x1'] < left_bound:
-            #         message = "카메라를 왼쪽으로 옮기십시오"
-            #     elif bbox['x2'] > right_bound:
-            #         message = "카메라를 오른쪽으로 옮기십시오"
-            #     elif bbox['y1'] < top_bound:
-            #         message = "카메라를 위로 옮기십시오"
-            #     elif bbox['y2'] > bottom_bound:
-            #         message = "카메라를 아래로 옮기십시오"
-            #     else:
-            #         message = "피사체가 중앙에 있습니다"
+            if bbox['conf'] > 0.7:
+                # 5% margin
+                margin = 0.05
+                left_bound = img_width * margin
+                right_bound = img_width * (1 - margin)
+                top_bound = img_height * margin
+                bottom_bound = img_height * (1 - margin)
+
+                if bbox['x1'] < left_bound:
+                    message = "카메라를 왼쪽으로 옮기십시오"
+                elif bbox['x2'] > right_bound:
+                    message = "카메라를 오른쪽으로 옮기십시오"
+                elif bbox['y1'] < top_bound:
+                    message = "카메라를 위로 옮기십시오"
+                elif bbox['y2'] > bottom_bound:
+                    message = "카메라를 아래로 옮기십시오"
+                else:
+                    message = "촬영하세요"
 
                 # 이미지 표시
                 # cv2.imshow('Detection Results', img)
                 # cv2.waitKey(1)
                 # cv2.destroyAllWindows()
 
-            # else:
-            #     message = "얼굴 인식 정확도가 낮습니다. 카메라를 조정하십시오."
+            else:
+                message = "얼굴 인식 정확도가 낮습니다. 카메라를 조정하십시오."
 
         return {"message": message}
 
@@ -107,6 +107,7 @@ async def detect_multiple_whole_body(request: MultiFaceRequest):
         image_bytes = base64.b64decode(request.base64_image)
         image_np = np.frombuffer(image_bytes, np.uint8)
         img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        img_height, img_width, _ = img.shape
 
         if img is None:
             raise HTTPException(status_code=400, detail="이미지를 디코딩할 수 없습니다.")
@@ -139,19 +140,38 @@ async def detect_multiple_whole_body(request: MultiFaceRequest):
         plen = len(boxes)
         if plen == 0:
             message = "얼굴을 찾을 수 없습니다. 카메라를 조정하십시오."
+        elif len(boxes) != request.required_faces:
+            message = f"화면에 {len(boxes)}명이 있습니다."
         else:
             sorted(boxes, key=lambda x: x['x'], reverse=True)
-            bbox = result.boxes[0]
-            message = f"화면에 {len(boxes)}명이 있습니다."
 
-            for box in boxes:
-                x1, y1, x2, y2 = box['x1'], box['x2'], box['y1'], box['y2']
-                # label = f"{class_id}: {conf:.2f}"  # 레이블 텍스트
-                # print(x1, ", ", y1, ", ", x2, ", ", y2)
+            # 5% margin
+            margin = 0.05
+            left_bound = img_width * margin
+            right_bound = img_width * (1 - margin)
+            top_bound = img_height * margin
+            bottom_bound = img_height * (1 - margin)
 
-                # 바운딩 박스
-                # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                # cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            for idx, bbox in enumerate(boxes):
+                direction_message = []
+
+                if bbox['x1'] < left_bound:
+                    direction_message.append("오른쪽으로")
+                elif bbox['x2'] > right_bound:
+                    direction_message.append("왼쪽으로")
+
+                if bbox['y1'] < top_bound:
+                    direction_message.append("아래로")
+                elif bbox['y2'] > bottom_bound:
+                    direction_message.append("위로")
+
+                if direction_message:
+                    message.append(f"왼쪽에서 {idx+1}번째 분 {' 그리고 '.join(direction_message)}")
+
+            if message:
+                message.append("이동해 주세요")
+            else:
+                message = "촬영하세요"
 
         return {"message": message}
 
